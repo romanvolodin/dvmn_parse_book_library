c1b2a8d53248c5ff51122556d7627a66fa4a59cd
Roman Volodin <roman.volodin@gmail.com>
2022-02-04 01:04:17 +0300

    Replace columns with flex

--- render.py
+++ render.py
@@ -2,7 +2,6 @@ import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
-from more_itertools import chunked
 
 
 def on_reload():
@@ -12,15 +11,12 @@ def on_reload():
     for book in books:
         book["img_src"] = book["img_src"].replace("scifi_books/", "")
 
-    books = list(chunked(books, 12))
-    books1, books2 = books
-
     env = Environment(
         loader=FileSystemLoader("./templates"),
         autoescape=select_autoescape(["html", "xml"]),
     )
     template = env.get_template("index.html")
-    page = template.render({"books1": books1, "books2": books2})
+    page = template.render({"books": books})
 
     with open("scifi_books/index.html", "w") as file:
         file.write(page)
