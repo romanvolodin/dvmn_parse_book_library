35a17c3d7c1ce5c14922902e0f9f296c3a53eb24
Roman Volodin <roman.volodin@gmail.com>
2022-02-03 20:36:38 +0300

    Add livereload

--- render.py
+++ render.py
@@ -1,15 +1,30 @@
 import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server, shell
 
 
-if __name__ == "__main__":
+def on_reload():
     with open("scifi_books/books.json", "r") as file:
         books = json.load(file)
+
+    for book in books:
+        book["img_src"] = book["img_src"].replace("scifi_books/", "")
 
     env = Environment(
         loader=FileSystemLoader("./templates"),
         autoescape=select_autoescape(["html", "xml"]),
     )
     template = env.get_template("index.html")
-    print(template.render({"books": books}))
+    page = template.render({"books": books})
+
+    with open("scifi_books/index.html", "w") as file:
+        file.write(page)
+
+
+if __name__ == "__main__":
+    on_reload()
+
+    server = Server()
+    server.watch("templates/*.html", shell("make html", cwd="docs"))
+    server.serve(root="scifi_books/")
